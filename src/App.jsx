d061f17636d5aef32f6215ccc962deefe76ce872
Bomowo <oeccc25@yahoo.com>
2023-09-08 17:25:52 +0300

    App fetch quote at load and make btn fetch quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [quote, setQuote] = useState({})
+  
+  useEffect(() => {
+    fetchNewQuote()
+  }, [])
+
+  function fetchNewQuote () {
+    fetch('https://api.quotable.io/quotes/random')
+    .then(response => response.json())
+    .then(json => {
+      setQuote({
+        text: json[0].content,
+        author: json[0].author
+      })
+    })
+    .catch(error => console.log(error))
+  }
 
 
   return (
     <div className="quote-box" id="quote-box">
 
       <div className="quote-container">
-        <h1 className="quote" id="text">Some random quote from famous person</h1>
-        <h2 className="quote-author" id="author">Name of said person</h2>
+        <h1 className="quote" id="text">{quote && quote.text}</h1>
+        <h2 className="quote-author" id="author">{quote && quote.author}</h2>
       </div>
 
       <div className="buttons-container">
         <button className="twitter-send" id="tweet-quote">Send that quote to twitter</button>
-        <button className="get-new-quote" id="new-quote">I want another fire quote</button>
+        <button className="get-new-quote" id="new-quote" onClick={fetchNewQuote}>I want another fire quote</button>
       </div>
 
     </div>
