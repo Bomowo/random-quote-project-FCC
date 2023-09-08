b0927ebc9391c73f79c5d151a3ddc7bdd6400242
Bomowo <oeccc25@yahoo.com>
2023-09-08 17:11:33 +0300

    Add design and do page mark-up

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ function App() {
 
 
   return (
-    <>
-     Hello, World
-    </>
+    <div className="quote-box" id="quote-box">
+
+      <div className="quote-container">
+        <h1 className="quote" id="text">Some random quote from famous person</h1>
+        <h2 className="quote-author" id="author">Name of said person</h2>
+      </div>
+
+      <div className="buttons-container">
+        <button className="twitter-send" id="tweet-quote">Send that quote to twitter</button>
+        <button className="get-new-quote" id="new-quote">I want another fire quote</button>
+      </div>
+
+    </div>
   )
 }
 
